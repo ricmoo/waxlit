7f355a03e638cf446ab611940f0a1332e2dd1595
Richard Moore <github@ricmoo.com>
2020-08-04 22:25:47 -0400

    Added initial spool support to article.

--- src.ts/article.ts
+++ src.ts/article.ts
@@ -1,5 +1,7 @@
+
 import aesjs from "aes-js";
 import { ethers } from "ethers";
+
 
 const _constructorGuard = {};
 
@@ -13,7 +15,28 @@ const MAGIC = ethers.utils.concat([
     ethers.utils.toUtf8Bytes("\n"),
 ]);
 
-console.log(MAGIC);
+const spoolAddress = "0x5C9bef28344Df7F7e67a1C5CF124dC707a973b07";
+
+const spoolAbi = [
+    "event NewArticle(bytes32 indexed nodehash, bytes32 info, bytes32 multihash)",
+    "function delegatedPostArticles(bytes32[] nodehashes, bytes32[] articleInfos, bytes32[] multihashes, tuple(bytes32 r, bytes32 vs)[] signatures)",
+    "function isAuthorized(bytes32 nodehash, address addr, uint256 gasLimit) view returns (bool)",
+    "function postArticles(bytes32[] nodehashes, bytes32[] articleInfos, bytes32[] multihashes)"
+];
+
+function getSpoolContract(provider: ethers.providers.Provider): ethers.Contract {
+    return new ethers.Contract(spoolAddress, spoolAbi, provider);
+}
+
+
+export interface ArticleInfo {
+    readonly articleId: number;
+    readonly revision: number;
+    readonly secretSalt: string;
+    readonly hash: string;
+    readonly blockNumber: number;
+    readonly transaction: string;
+}
 
 export class Article {
     readonly title: string;
@@ -35,8 +58,11 @@ export class Article {
         });
     }
 
-    encrypt(key: string): Uint8Array {
-        const baseSymKey = ethers.utils.id(key);
+    encrypt(key: ethers.utils.BytesLike): Uint8Array {
+        const keyBytes = ethers.utils.arrayify(key);
+        if (keyBytes.length !== 16) { throw new Error("invalid key"); }
+
+        const baseSymKey = ethers.utils.keccak256(keyBytes);
         const secretSalt = ethers.utils.hexlify(ethers.utils.randomBytes(16));
         const salt = ethers.utils.arrayify(ethers.utils.keccak256(secretSalt)).slice(0, 16);
         const symKey = ethers.utils.keccak256(ethers.utils.concat([ baseSymKey, salt ]));
@@ -48,17 +74,53 @@ export class Article {
         ]);
     }
 
-    save(key: string): Promise<string> {
+    async save(key: string): Promise<string> {
         const data = this.encrypt(key);
         console.log(data);
         return null;
+    }
+
+    static async listArticles(provider: ethers.providers.Provider, ensName: string): Promise<Array<ArticleInfo>> {
+        const contract = getSpoolContract(provider);
+
+        const articles: { [ articleId: number ]: ArticleInfo } = { };
+
+        const filter = contract.filters.NewArticle(ethers.utils.namehash(ensName));
+        const logs = await contract.queryFilter(filter, 0);
+
+        logs.forEach((log) => {
+            try {
+                const blockNumber = log.blockNumber;
+                const transaction = log.transactionHash;
+
+                const version = ethers.BigNumber.from(log.args[1]).shr(8 * 31).toNumber();
+                if (version !== 1) { throw new Error("unsupported version"); }
+
+                const secretSalt = ethers.utils.hexZeroPad(ethers.BigNumber.from(log.args[1]).shr(8 * 10).mask(8 * 16).toHexString(), 16);
+                const articleId = ethers.BigNumber.from(log.args[1]).shr(8 * 4).mask(8 * 6).toNumber();
+                const revision = ethers.BigNumber.from(log.args[1]).shr(8 * 0).mask(8 * 4).toNumber();
+                const hashBin = ethers.utils.concat([ [ 18, 32 ], log.args[2] ]);
+                const hash = ethers.utils.base58.encode(hashBin);
+
+                if (articles[articleId] == null || articles[articleId].revision < revision) {
+                    articles[articleId] = { articleId, blockNumber, hash, transaction, revision, secretSalt };
+                }
+            } catch (error) {
+                console.log("Error processing article entry", error);
+            }
+        });
+
+        const result = Object.values(articles).filter((ai) => !ethers.BigNumber.from(ai.secretSalt).isZero());
+        result.sort((a, b) => (a.articleId - b.articleId));
+
+        return result;
     }
 
     static from(title: string, body: string): Article {
         return new Article(_constructorGuard, title, body);
     }
 
-    static decrypt(key: string, data: Uint8Array): Article {
+    static decrypt(key: ethers.utils.BytesLike, data: Uint8Array): Article {
         // Check the Magic Number
         try {
             for (let i = 0; i < MAGIC.length; i++) {
@@ -68,7 +130,10 @@ export class Article {
             throw new Error("invalid waxLit article magic number");
         }
 
-        const baseSymKey = ethers.utils.id(key);
+        const keyBytes = ethers.utils.arrayify(key);
+        if (keyBytes.length !== 16) { throw new Error("invalid key"); }
+
+        const baseSymKey = ethers.utils.keccak256(keyBytes);
         const salt = data.slice(MAGIC.length, MAGIC.length + 16);
         const symKey = ethers.utils.keccak256(ethers.utils.concat([ baseSymKey, salt ]));
         const aes = new aesjs.ModeOfOperation.ctr(ethers.utils.arrayify(symKey), new aesjs.Counter(salt));
@@ -91,7 +156,15 @@ export class Article {
     }
 }
 
-const art = Article.from("Hello World", "My body, my rules");
-const data = art.encrypt("01234567")
-const orig = Article.decrypt("01234567", data);
-console.log(orig);
+(async function() {
+    const key = "0x01234567012345670123456701234567";
+    const art = Article.from("Hello World", "My body, my rules");
+    const hash = await art.save(key);
+    console.log(hash);
+    const data = art.encrypt(key)
+    const orig = Article.decrypt(key, data);
+    console.log(ethers.utils.hexlify(data), orig);
+
+    const articles = await Article.listArticles(ethers.getDefaultProvider("ropsten"), "ricmoose.eth");
+    console.log(articles);
+})();
